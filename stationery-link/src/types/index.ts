999494e55d5fcce2ef0bffe47ecc45ba77b29cbb
GriffithMoss <griffithmoss@outlook.com>
2025-07-15 09:31:50 +0900

    refresh

--- stationery-link/src/types/index.ts
+++
@@ -1,9 +0,0 @@
-export type Product = {
-  id: number;
-  created_at: string;
-  name: string;
-  description: string | null; // Can be null
-  price: number;
-  image_url: string | null; // Can be null
-  category: string | null; // Can be null
-};
